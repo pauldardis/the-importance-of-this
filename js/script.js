141ce55b03847a4411c4e18fe227b0d87538d446
Ubuntu <ubuntu@ip-172-31-83-186.ec2.internal>
2019-10-04 11:47:50 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$("button").mouseenter(function() {
+    $(this).removeClass("makeRed").addClass("makeBlue");
+})
+
+$("button").mouseleave(function() {
+    $(this).removeClass("makeBlue").addClass("makeRed");
+})
+
+
